ebbc3944f261a804d67e4b018d7af7ad0c09ff3f
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-03-21 13:55:11 +0200

    viikko2 tehtävä 6 ja 7

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,30 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_alkusaldo_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+    
+    def test_saldon_lataaminen_onnistuu(self):
+        self.maksukortti.lataa_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 15)
+    
+    def test_saldo_vähenee_oikein_jos_rahaa(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5)
+    
+    def test_saldo_ei_muutu_jos_kortilla_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+
+    def test_ota_rahaa_palauttaa_true_jos_onnistuu(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+    
+    def test_ota_rahaa_palauttaa_false_jos_epäonnistuu(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
+    
+    def test_kortin_rahasumma_tulostuu_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
