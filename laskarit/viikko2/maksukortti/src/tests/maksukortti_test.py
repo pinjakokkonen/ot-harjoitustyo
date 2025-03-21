c84e01bb6d132584f4d90b9b3ca4d746a26d0f11
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-03-21 11:29:12 +0200

    viikko2 tehtävä 3

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -34,3 +34,26 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti.lataa_rahaa(20000)
 
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+    
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+    
+    def test_kortille_ei_voi_ladata_negatiivista_arvoa(self):
+        self.kortti.lataa_rahaa(-500)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 10)
+    
+    def test_syo_edullisesti_kun_rahaa_sen_verran(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0)
+    
+    def test_syo_maukkaasti_kun_rahaa_sen_verran(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0)
