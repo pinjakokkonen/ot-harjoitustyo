459f9ee3ec1ef0bc466a4227bc8aa8d9452c52c5
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-04-23 15:49:54 +0300

    add view to choose color

--- src/services/uno_service.py
+++ src/services/uno_service.py
@@ -38,6 +38,8 @@ class UnoService:
 
     def play_card(self, card):
         action_card = True
+        if card=="":
+            return
         if card[0] not in self.actions:
             action_card = False
         if card not in self.actions[3:]:
@@ -67,7 +69,7 @@ class UnoService:
             self.play_action_card(card)
 
     def play_action_card(self, card):
-        if card[0] == "r" or card[0] == "s":
+        if card[0] in ["r", "s"] or card == "wild":
             self.skip_turn()
         elif card[0] == "d":
             if self.turn == "player1":
@@ -87,9 +89,7 @@ class UnoService:
                 for _ in range(4):
                     draw = self.deck.pop()
                     self.player2.append(draw)
-        else:
-            # wild
-            pass
+            self.skip_turn()
 
     def skip_turn(self):
         if self.turn == "player1":
@@ -120,3 +120,7 @@ class UnoService:
                 self.player2.remove(i)
                 self.turn = "player1"
                 self.check_action_card(action_card, i)
+
+    def choose_color(self, color):
+        self.stack = ("-", color)
+        self.skip_turn()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -9,12 +9,15 @@ class UI:
     def start(self):
         self.view()
         self.service.start_game()
-        self.update_view()
+        self.playing_view()
+        self.frame.pack()
     
     def view(self):
-        self.first_line = ttk.Frame(self._root)
+        self.frame = ttk.Frame(self._root)
+
+        self.first_line = ttk.Frame(self.frame)
         self.first_line.pack(pady=20)
-        self.second_frame = ttk.Frame(self._root)
+        self.second_frame = ttk.Frame(self.frame)
         self.second_frame.pack(pady=20)
 
         self.middle = ttk.LabelFrame(self.first_line, text="middle deck")
@@ -34,26 +37,18 @@ class UI:
 
         self.player2_label = ttk.Label(self.player2, text="")
         self.player2_label.pack(pady=20)
-        
-        self.turn = ttk.Label(self._root, text="")
-        self.turn.pack()
-
-        self._entry = ttk.Entry(self._root)
-        self._entry.pack()
-
-        button = ttk.Button(self._root, text="Play card", command=self._handle_button_click_play)
-        button.pack()
-
-        button = ttk.Button(self._root, text="Draw a card", command=self._handle_button_click_draw)
-        button.pack()
-
-        actions = ttk.Label(self._root, text="r=reverse, s=skip, d=draw two")
-        actions.pack()
 
     def _handle_button_click_play(self):
         entry = self._entry.get()
         self.service.play_card(entry)
-        self.update_view()
+        if entry == "wild" or entry == "wild draw four":
+            self.destroy_view()
+            self.view()
+            self.choose_color_view()
+            self.update_view()
+            self.frame.pack()
+        else:
+            self.update_view()
 
     def update_view(self):
         self.middle_label.config(text=self.service.stack)
@@ -67,3 +62,50 @@ class UI:
     def _handle_button_click_draw(self):
         self.service.draw_a_card()
         self.update_view()
+
+    def destroy_view(self):
+        self.frame.pack_forget()
+
+    def playing_view(self):
+        self.turn = ttk.Label(self.frame, text="")
+        self.turn.pack()
+
+        self._entry = ttk.Entry(self.frame)
+        self._entry.pack()
+
+        button = ttk.Button(self.frame, text="Play card", command=self._handle_button_click_play)
+        button.pack()
+
+        button = ttk.Button(self.frame, text="Draw a card", command=self._handle_button_click_draw)
+        button.pack()
+
+        actions = ttk.Label(self.frame, text="r=reverse, s=skip, d=draw two")
+        actions.pack()
+        self.update_view()
+
+    def choose_color_view(self):
+        self.turn = ttk.Label(self.frame, text="")
+        self.turn.pack()
+
+        self.choose = ttk.Label(self.frame, text="Choose color")
+        self.choose.pack()
+
+        red = ttk.Button(self.frame, text="red", command=lambda:self._handle_button_click_choose_color("red"))
+        red.pack()
+
+        green = ttk.Button(self.frame, text="green", command=lambda:self._handle_button_click_choose_color("green"))
+        green.pack()
+
+        blue = ttk.Button(self.frame, text="blue", command=lambda:self._handle_button_click_choose_color("blue"))
+        blue.pack()
+
+        yellow = ttk.Button(self.frame, text="yellow", command=lambda:self._handle_button_click_choose_color("yellow"))
+        yellow.pack()
+    
+    def _handle_button_click_choose_color(self, button_text):
+        chosen_color = button_text
+        self.service.choose_color(chosen_color)
+        self.destroy_view()
+        self.view()
+        self.playing_view()
+        self.frame.pack()
